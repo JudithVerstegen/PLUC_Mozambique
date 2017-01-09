e4c374ab75735aae8edd757a7c174d91fe9c0c4b
Judith Verstegen <judithverstegen@gmail.com>
2017-01-09 12:27:13 +0100

    turned calculation of percentiles on, which was turned off for computation time reduction but does belong to the original model

--- model/LU_Moz.py
+++ model/LU_Moz.py
@@ -873,7 +873,7 @@ class LandUseChangeModel(DynamicModel, MonteCarloModel):
       percentiles = [0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]
       mcaveragevariance(names, sampleNumbers, timeSteps)
       names = ['eY', 'eYPr', 'eYTo']
-##      mcpercentiles(names, percentiles, sampleNumbers, timeSteps)
+      mcpercentiles(names, percentiles, sampleNumbers, timeSteps)
 
     print '\n...done'
 
